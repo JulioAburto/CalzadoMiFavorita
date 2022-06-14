ea9c7e12037d1961bda72b06d09ab618f67884a8
Jose J Castillo <chucas115@gmail.com>
2022-06-13 20:22:58 -0600

    Avance app

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+import { LoginView } from "./components/view/LoginView";
+import { MainView } from "./components/view/MainView";
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Calzado Mi Favorita</Text>
-			<Text>Sistema de Facturacion</Text>
-			<Text>Computadora Julio a Computadora Chucas</Text>
-			<StatusBar style="auto" />
-		</View>
+
+		<NavigationContainer>
+			<Stack.Navigator screenOptions={{ headerShown: false }}>
+				<Stack.Screen name="LoginView" component={LoginView} />
+				<Stack.Screen name="MainView" component={MainView} /> 
+			</Stack.Navigator>
+		</NavigationContainer>
+
+		// <View style={styles.container}>
+		// 	<Text>Calzado Mi Favorita</Text>
+		// 	<Text>Sistema de Facturacion</Text>
+		// 	<Text>Computadora Julio a Computadora Chucas</Text>
+		// 	<StatusBar style="auto" />
+		// </View>
 	);
 }
 
