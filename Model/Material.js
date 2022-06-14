100e43f0f87a517ce21a8637a8e1eb504972134c
Julio <juliocesaraburtoguido@gmail.com>
2022-06-13 19:28:41 -0600

    añadiendo estilos

--- Model/Material.js
+++ Model/Material.js
@@ -1,17 +1,17 @@
 class Material {
-  constructor(
-    material = {
-      Id_Material: "",
-      NombreMaterial: "",
-      Fecha_Registro: "",
-    }
-  ) {
-    for (const prop in material) {
-      this[prop] = material[prop];
-    }
-  }
-  Id_Material = "";
-  NombreMaterial = "";
-  Fecha_Registro = "";
+	constructor(
+		material = {
+			Id_Material: "",
+			NombreMaterial: "",
+			Fecha_Registro: "",
+		}
+	) {
+		for (const prop in material) {
+			this[prop] = material[prop];
+		}
+	}
+	Id_Material = "";
+	NombreMaterial = "";
+	Fecha_Registro = "";
 }
 export { Material };
