ac0e8913c3e690aa3b4d830bdc86d9f90d472263
Jose J Castillo <chucas115@gmail.com>
2022-06-14 20:52:11 -0600

    Completando del Get y TakeData

--- Model/core/Entity.js
+++ Model/core/Entity.js
@@ -1,3 +1,4 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { ApiDatabase } from "./ApiDatabase";
 class Entity {
 	constructor(props) {
@@ -11,6 +12,42 @@ class Entity {
 		Set: "ApiSet",
 		Update: "ApiUpdate",
 	};
-	Get = async (Param = "") => {};
+	Get = async (Param = "") => {
+		let Data = await this.TakeData();
+		Data = Data.filter((ent) => {
+			let flag = false; 
+			for (const prop in ent) {
+				if (
+					ent[prop].__proto__ != Array.prototype &&
+					ent[prop].__proto__ != Object.prototype &&
+					ent[prop].__proto__ != Function.prototype &&
+					ent[prop].toString().toUpperCase().includes(Param.toUpperCase())
+				){
+					flag = true;
+				}
+			}
+			return flag;
+		});
+		return Data.map((ent) => new this.constructor(ent));
+	};
+
+	async TakeData() {
+		let Data = [];
+		const DataBase = await AsyncStorage.getItem("@" + this.ApiMethods.Get);
+		if (DataBase == null){
+			Data = ApiDatabase[this.ApiMethods.Get];
+			await AsyncStorage.setItem("@" + this.ApiMethods.Get, JSON.stringify(Data, this.replacer));
+		} else {
+			Data = JSON.parse(DataBase);
+		}
+		return Data;
+	}
+
+	replacer(key, value) {
+		if (value.get && value.set) {
+			return undefined;
+		}
+		return value;
+	}
 }
 export { Entity };
