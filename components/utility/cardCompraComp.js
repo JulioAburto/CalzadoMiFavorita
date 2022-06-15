6b63705c3684213000433bd67ce9ed24b4e47993
Jose J Castillo <chucas115@gmail.com>
2022-06-14 21:44:16 -0600

    Avance de VistaCompras

--- components/utility/cardCompraComp.js
+++ components/utility/cardCompraComp.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Button, Text, View } from "react-native";
 
-const cardCompraComp = (props) => {
+const CardCompraComp = (props) => {
 	return (
 		<View style={styles.CardStyle}>
 			<Text style={styles.Title}>Factura Compra</Text>
@@ -8,12 +8,23 @@ const cardCompraComp = (props) => {
 			<Text style={styles.Atribute}>Compra: {props.data.Id_Compra} </Text>
 			{/* <Text style={styles.Resumen}>Resumen: {props.data.ResumenCurso} </Text> */}
 			<Text style={styles.Atribute}>Usuario: {props.data.Id_Usuario} </Text>
-			
+			<Button
+				title="Ver Detalle Cursos"
+				onPress={() => {
+					//props.cargarBloques(props.data);
+				}}
+			></Button>
+			<Button
+				title="Ver Usuarios Matriculados"
+				onPress={() => {
+					//props.cargar(props.data);
+				}}
+			></Button>
 		</View>
 	);
 };
 
-export { cardCompraComp };
+export { CardCompraComp };
 
 const styles = StyleSheet.create({
 	CardStyle: {
--- components/view/CompraView.js
+++ components/view/CompraView.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { StyleSheet, Text, Button, ActivityIndicator, TextInput, ScrollView } from "react-native";
 import { Compra_Articulo } from "../../Model/Compra_Articulo";
-import { cardCompraComp } from "../utility/cardCompraComp";
+import { CardCompraComp } from "../utility/CardCompraComp";
 class CompraView extends React.Component {
 	constructor(props) {
 		super();
+		this.props = props;
 		this.state = {
 			isLoading: true,
 			Dataset: [],
 		};
 		this.Compra = new Compra_Articulo();
+		this.CargarCompra();
 	}
 	CargarCompra = async (param = "") => {
 		const Compras = await this.Compra.Get(param);
@@ -22,13 +24,24 @@ class CompraView extends React.Component {
 		return (
 			<ScrollView>
 				<Text>Facturas de las compras</Text>
+				<Button
+				title="Nueva Compra"
+				onPress={()=>
+					this.props.navigation.navigate(this.CargarCompra)
+				}
+				></Button>
+				<TextInput
+				style={{ padding: 10, margin: 10, borderWidth: 1, borderRadius: 3 }}
+				placeholder="Buscar"
+				onChangeText={(val) => this.CargarCompra(val)}
+				></TextInput>
                 {this.state.isLoading ? (
 					<ActivityIndicator />
 				) : (
-					this.state.Dataset.map((curso) => (
-						<cardCompraComp
-							key={curso.IdCurso}
-							data={curso}
+					this.state.Dataset.map((compra) => (
+						<CardCompraComp
+							key={compra.Id_Compra}
+							data={compra}
 						/>
 					))
 				)}
